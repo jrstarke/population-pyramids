2c1a3c1e58433ea46c24c34a6eb725c69cfa88ef
Lars Grammel <lars.grammel@gmail.com>
2012-07-01 19:34:29 -0700

    added title better chart alignment (centered) removed unused csv file

--- public/population_pyramid.js
+++ public/population_pyramid.js
@@ -2,7 +2,7 @@ function renderChart(data, compareData) {
 
     // chart settings
     var margin = {top: 60, right: 10, bottom: 20, left: 10};
-    var outerWidth = 860;
+    var outerWidth = 940;
     var outerHeight = 800;
     var width = outerWidth - margin.left - margin.right;
     var height = outerHeight - margin.top - margin.bottom;
