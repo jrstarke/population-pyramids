17d623214c6b2fbbf41b88b52469192e3dd1d3ac
Lars Grammel <lars.grammel@gmail.com>
2012-07-02 17:11:06 -0700

    fixed popover alignment

--- public/lib/cpopover.js
+++ public/lib/cpopover.js
@@ -23,7 +23,7 @@
         constructor: CirclePopover
         , getPosition: function (inside) {
             return $.extend({}, (inside ? {top: 0, left: 0} : this.$element.offset()), {
-                width: 840, height: 4
+                width: 920, height: 4
             })
         }
     })
